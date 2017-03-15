0823b9085ca1801a8e8b840b22aa449a6b969ef3
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:38:45 -0300

    adicionado bootstrap adiciona-produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,10 +1,22 @@
 <html>
-<?php
-$nome = $_GET['nome'];
-$preco = $_GET['preco'];
+	<head>
+		<title>Minha Loja</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/loja.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="principal">
+				<?php
+				$nome = $_GET['nome'];
+				$preco = $_GET['preco'];
 
-?>
+				?>
 
 
-<h1>Produto <?=$nome?>, <?=$preco?> adicionado com sucesso!</h1>
+					<p class="alert-success">Produto <?=$nome?>, <?=$preco?> adicionado com sucesso!</p>
+			</div>
+		</div>
+	</body>
 </html>
