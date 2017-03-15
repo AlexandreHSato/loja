6c76ae58b2bbdd552645cba4ec7313fc445019d5
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:34:18 -0300

    adicionado bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-<h1>Bem vindo!</h1>
-</body>
+	<head>
+		<title>Minha Loja</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/loja.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="principal">
+				<h1>Bem vindo!</h1>
+			</div>
+		</div>
+	</body>
 </html>
