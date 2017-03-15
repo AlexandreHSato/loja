965cd72daf1267d9ca7972395200d41291749ce7
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:54:18 -0300

    adicionando rodape e cabecalho adiciona-produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,13 +1,4 @@
-<html>
-	<head>
-		<title>Minha Loja</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/loja.css">
-	</head>
-	<body>
-		<div class="container">
-			<div class="principal">
+<?php include("cabecalho.php");?>
 				<?php
 				$nome = $_GET['nome'];
 				$preco = $_GET['preco'];
@@ -16,7 +7,4 @@
 
 
 					<p class="alert-success">Produto <?=$nome?>, <?=$preco?> adicionado com sucesso!</p>
-			</div>
-		</div>
-	</body>
-</html>
+<?php include("rodape.php");?>
