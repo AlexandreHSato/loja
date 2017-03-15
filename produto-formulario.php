bc53c1c02423ade6e525c4e3eca0c1b5e10a887a
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:17:17 -0300

    adiciona formulario produto

---
+++ produto-formulario.php
@@ -0,0 +1,12 @@
+<html>
+
+	<form>
+		Nome:
+		<input type="text" name="nome"><br/>
+		Preco:
+		<input type="number" name="preco" > <br/>
+
+		<input type="submit" value="Cadastrar">
+
+	</form>
+</html>
