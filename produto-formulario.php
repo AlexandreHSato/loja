5f483215a727632e2749d0bd634547f2f8a0857e
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:54:52 -0300

    adicionando rodape e cabecalho formulario-produto

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,13 +1,4 @@
-<html>
-	<head>
-		<title>Minha Loja</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/loja.css">
-	</head>
-	<body>
-		<div class="container">
-			<div class="principal">
+<?php include("cabecalho.php");?>
 				<h1>Formulario de Produtos</h1>
 				<form action="adiciona-produto.php">
 					Nome:
@@ -18,7 +9,4 @@
 					<input type="submit" value="Cadastrar">
 
 				</form>
-			</div>
-		</div>
-	</body>
-</html>
+<?php include("rodape.php");?>
