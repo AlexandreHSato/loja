cb9b3b52f4b424cdec6027974e8a00551495225c
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:41:36 -0300

    adicionado bootstrap produto-formulario

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,12 +1,24 @@
 <html>
+	<head>
+		<title>Minha Loja</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/loja.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="principal">
+				<h1>Formulario de Produtos</h1>
+				<form action="adiciona-produto.php">
+					Nome:
+					<input type="text" name="nome"><br/>
+					Preco:
+					<input type="number" name="preco" > <br/>
 
-	<form action="adiciona-produto.php">
-		Nome:
-		<input type="text" name="nome"><br/>
-		Preco:
-		<input type="number" name="preco" > <br/>
+					<input type="submit" value="Cadastrar">
 
-		<input type="submit" value="Cadastrar">
-
-	</form>
+				</form>
+			</div>
+		</div>
+	</body>
 </html>
