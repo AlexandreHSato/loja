dc70fefee6e9f3bb00abb3e176b5092700119011
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-14 22:55:15 -0300

    adicionando rodape e cabecalho index

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<html>
-	<head>
-		<title>Minha Loja</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/loja.css">
-	</head>
-	<body>
-		<div class="container">
-			<div class="principal">
+<?php include("cabecalho.php");?>
 				<h1>Bem vindo!</h1>
-			</div>
-		</div>
-	</body>
-</html>
+<?php include("rodape.php");?>
