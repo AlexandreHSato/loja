ea259d1452c3dead37c4e54474b899b17cf42d19
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-19 20:37:00 -0300

    cricando funcao altera produto

--- banco-produto.php
+++ banco-produto.php
@@ -13,13 +13,21 @@ function listaProdutos($conexao){
 function inseriProduto($conexao, $nome, $preco,$descricao,$categoria,$usado){
 	$query = "INSERT INTO produtos (nome,preco,descricao,categoria_id,usado) VALUES ('{$nome}',{$preco},'{$descricao}',{$categoria},{$usado});";
 	return mysqli_query($conexao,$query);
-
 }
 
+function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria, $usado){
+	$query = "UPDATE produtos SET  nome = '{$nome}', preco = {$preco}, descricao = '{$descricao}', categoria_id = {$categoria}, usado = {$usado} WHERE id = {$id}";
+	return mysqli_query($conexao,$query);
+	
+}
+
+function buscaProduto($conexao, $id){	
+	$query = "SELECT * FROM produtos WHERE id={$id};";
+	$resultado =  mysqli_query($conexao,$query);
+	return mysqli_fetch_assoc($resultado);
+}
 
 function removeProduto($conexao, $id){
-	
 	$query = "DELETE FROM produtos WHERE id={$id};";
 	return mysqli_query($conexao,$query);
-
 }
