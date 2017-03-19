446e68944ca5c080df560dcfdac79fab64897a92
Alexandre Sato <alexandrehsato@gmail.com>
2017-03-19 17:32:59 -0300

    acrescentando produto-lista no menu

--- cabecalho.php
+++ cabecalho.php
@@ -14,6 +14,8 @@
 				<div >
 					<ul class="nav navbar-nav">
 						<li> <a href="produto-formulario.php">Adicionar Produto</a></li>
+						<li> <a href="produto-lista.php">Produtos</a></li>
+						<li> <a href="">Sobre</a></li>
 					</ul>
 				</div>
 			</div>
